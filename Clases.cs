897411a631ecccf1f613f1ee039c1b9a7bcfb130
Diego Apugliese <dapugliese@gmail.com>
2024-06-11 21:57:34 -0300

    version 1

---
+++ Clases.cs
@@ -0,0 +1,27 @@
+
+namespace libreriaClases{
+
+    class perro{
+
+       public string raza;
+        public int edad;
+        public string tamano;
+
+
+       public void jugar(string pJugar) {
+
+            Console.WriteLine("Jugando......" + pJugar);
+
+        }
+
+        public string ladrar() {
+
+            return  "guau guau....";
+
+        }
+
+    }
+}
+
+
+
---
+++ Principal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using libreriaClases;
+namespace principal {
+    class programaPrincipal{
+            static public void Main(){
+                
+                string nombre;
+                nombre = "juan";
+                perro firulai;
+
+                firulai = new perro();
+
+                perro pedro = new perro();
+
+                Console.WriteLine("Hola");
+
+                firulai.edad = 2;
+
+                firulai.tamano = "grande";
+
+                firulai.raza = "caniche";
+
+                firulai.jugar("saltar");
+                firulai.jugar("domirse");
+
+                Console.WriteLine(firulai.ladrar());
+
+            }
+    }
+}
